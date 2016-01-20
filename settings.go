1ae053810a0cfd21f7eb619699053d33eaeee766
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-20 22:29:59 +0000

    settings

--- settings.go
+++ settings.go
@@ -1,1 +1,39 @@
-package no_time_to_explain
+package main
+
+import (
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// Configuration - it is what it is
+type Configuration struct {
+	port           string
+	appKey         string
+	databaseName   string
+	meetupEndpoint string
+}
+
+// DefaultPort - as name suggest
+const DefaultPort = "8080"
+
+// DefaultDatabaseName - default database name that will be created
+const DefaultDatabaseName = "requests.db"
+
+const DefaultMeetupEndpoint = "https://api.meetup.com/2/"
+
+func InitSettings() *Configuration {
+	var appConfig Configuration
+
+	appConfig.port = DefaultPort
+	appConfig.databaseName = DefaultDatabaseName
+	appConfig.meetupEndpoint = DefaultMeetupEndpoint
+
+	appConfig.appKey = os.Getenv("MeetupKey")
+
+	if appConfig.appKey == "" {
+		log.Fatal("Failed to get Meetup API key")
+	}
+
+	return &appConfig
+}
