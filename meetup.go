038f5243927ccca2d3436e7546276c4a19055630
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-20 22:29:51 +0000

    meetup lib

--- meetup.go
+++ meetup.go
@@ -1,1 +1,93 @@
-package no_time_to_explain
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+// DBClient provides access to cache, http client and configuration
+type Handler struct {
+	http *http.Client
+	cfg  *Configuration
+}
+
+// Member struct holds information about each member
+type Member struct {
+	ID      int     `json:"id"`
+	Name    string  `json:"name"`
+	Link    string  `json:"link"`
+	Country string  `json:"country"`
+	City    string  `json:"city"`
+	Joined  int     `json:"joined"`
+	Lon     float64 `json:"lon"`
+	Lat     float64 `json:"lat"`
+	Visited int     `json:"visited"`
+	Status  string  `json:"status"`
+}
+
+// ResponseMeta holds vital information for navigating through members API
+type ResponseMeta struct {
+	Next       string `json:"next"` // next URL to get more members
+	TotalCount int    `json:"total_count"`
+	Count      int    `json:"count"`
+}
+
+// MembersResponse - this is top level structure returned by API
+type MembersResponse struct {
+	Meta    ResponseMeta `json:"meta"`
+	Results []Member     `json:"results"`
+}
+
+func (h *Handler) getMembers(groupName string, pageSize int) ([]Member, error) {
+	//	https://api.meetup.com/2/members?group_urlname=frontend&page=200&key=343f7567781b654151e2c635c5445a&order=name
+	url := fmt.Sprintf("%smembers?group_urlname=%s&page=%d&key=%s", h.cfg.meetupEndpoint, groupName, pageSize, h.cfg.appKey)
+
+	var members []Member
+
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+			"url":   url,
+			"key":   h.cfg.appKey,
+		}).Error("failed to create request")
+		return members, err
+	}
+
+	resp, err := h.http.Do(request)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+			"url":   url,
+			"key":   h.cfg.appKey,
+		}).Error("failed to query API")
+		return members, err
+	}
+
+	mr := MembersResponse{}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+			"url":   url,
+			"key":   h.cfg.appKey,
+		}).Error("failed to read body")
+		return members, err
+	}
+
+	err = json.Unmarshal(body, &mr)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+			"url":   url,
+			"key":   h.cfg.appKey,
+		}).Error("failed to unmarshal response from API")
+		return members, err
+	}
+
+	return mr.Results, nil
+}
