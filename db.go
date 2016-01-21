a11d103b8f3863e93a58da1dd57a580dab1882c8
Karolis Rusenas <karolis.rusenas@gmail.com>
2016-01-21 22:50:41 +0000

    add quad functionality

--- db.go
+++ db.go
@@ -23,3 +23,28 @@ func InitDB() *cayley.Handle {
 
 	return db
 }
+
+// addQuad - adds quad to graph. In our case - it abstract graph database
+// so if something bad happens - we can swap it to different DB.
+// [subject] ---predicate---> [object]
+func (h *Handler) addQuad(subject, predicate, object string) (err error) {
+
+	quad := cayley.Quad(subject, predicate, object, "")
+	err = h.g.AddQuad(quad)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":     err.Error(),
+			"subject":   subject,
+			"predicate": predicate,
+			"object":    object,
+		}).Error("failed to add quad")
+		return
+	}
+	log.WithFields(log.Fields{
+		"subject":   subject,
+		"predicate": predicate,
+		"object":    object,
+	}).Info("quad added")
+
+	return
+}
